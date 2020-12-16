1201e1c0a30abdab4a90221563f6f7ac1f317587
batovpasha <batovpasha@gmail.com>
2020-12-17 01:54:20 +0200

    add third player initialization and cracking better Mersenne Twister PRNG

--- 3-lab/index.js
+++ 3-lab/index.js
@@ -13,10 +13,15 @@ async function crackCasinoRoyale() {
     // await player1.createAccount();
     // await player1.crackLcg();
 
-    const player2 = new CasinoPlayer(uuidv4(), casinoApi);
+    // const player2 = new CasinoPlayer(uuidv4(), casinoApi);
+    //
+    // await player2.createAccount();
+    // await player2.crackMt();
 
-    await player2.createAccount();
-    await player2.crackMt();
+    const player3 = new CasinoPlayer(uuidv4(), casinoApi);
+
+    await player3.createAccount();
+    await player3.crackBetterMt();
 }
 
 crackCasinoRoyale();
