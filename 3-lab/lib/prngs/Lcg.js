d8011cbc9179c214d3ff00eb4cd72d09995e61e9
batovpasha <batovpasha@gmail.com>
2020-12-17 00:22:02 +0200

    implement methods for cracking multiplier and increment, implement iterable protocol

--- 3-lab/lib/prngs/Lcg.js
+++ 3-lab/lib/prngs/Lcg.js
@@ -1,9 +1,37 @@
+const { mod, modinv } = require('../utils');
+
 class Lcg {
     constructor({ seed, multiplier, increment, modulus }) {
         this.state = BigInt(seed);
         this.multiplier = BigInt(multiplier);
         this.increment = BigInt(increment);
         this.modulus = modulus;
+    }
+
+    static crackUnknownMultiplier(states, modulus) {
+        return parseInt(
+            mod(
+                BigInt(states[2] - states[1]) * BigInt(modinv(states[1] - states[0], modulus)),
+                BigInt(modulus)
+            )
+        );
+    }
+
+    static crackUnknownIncrement(states, modulus, multiplier) {
+        return mod(states[1] - states[0] * multiplier, modulus);
+    }
+
+    [Symbol.iterator]() {
+        return {
+            next : () => {
+                this.state = mod((this.state * this.multiplier + this.increment), this.modulus);
+
+                return {
+                    value : this.state,
+                    done  : false // infinity iterator
+                };
+            }
+        }
     }
 }
 
