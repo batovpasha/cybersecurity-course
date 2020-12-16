890f0c905f2f51c1114fc3b72da9d106fffa9191
batovpasha <batovpasha@gmail.com>
2020-12-16 23:59:37 +0200

    add money field, implement method for creating player account

--- 3-lab/lib/CasinoPlayer.js
+++ 3-lab/lib/CasinoPlayer.js
@@ -2,6 +2,13 @@ class CasinoPlayer {
     constructor(id, casinoApi) {
         this.id = id;
         this.casinoApi = casinoApi;
+        this.money = 0;
+    }
+
+    async createAccout() {
+        const { money } = await this.casinoApi.createAccount(this.id);
+
+        this.money += money;
     }
 }
 
